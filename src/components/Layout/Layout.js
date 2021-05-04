804fa40aeb907221830e6d7344f220176978bc8c
junaidkhan777 <junaidilyaskhan777@gmail.com>
2021-05-04 11:35:59 +0500

    section 8 complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Auxilary from "../../hoc/Auxilary";
 import styles from './Layout.module.css'
 import ToolBar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout=(props)=>{
+class Layout extends Component{
 
-return(
-    <Auxilary>
+    state={
+        showSideDrawer:true
+    }
 
-    <ToolBar/>
-    <SideDrawer/>
-    <main className={styles.Content}>
-        {props.children}
-    </main>
-    </Auxilary> 
-); 
+  sideDrawerClosedHandler=()=>{
+   this.setState({showSideDrawer:false});
+  }
+
+
+  sideDrawerToggleHandler=()=>{
+      this.setState((prevState)=>{
+          return {showSideDrawer:!prevState.showSideDrawer}
+      })
+  }
+
+    render(){
+
+    return(
+        <Auxilary>
+    
+        <ToolBar DrawerToggleClicked={this.sideDrawerToggleHandler}/>
+
+        <SideDrawer 
+        open={this.state.showSideDrawer}
+        closed={this.sideDrawerClosedHandler}
+        />
+        
+        <main className={styles.Content}>
+            {this.props.children}
+        </main>
+        </Auxilary> 
+    );
+}
+ 
 }
 
 
-export default layout
+export default Layout
